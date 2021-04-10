2b2c0a9592d4f5493feaf7919aabb395343a3e57
SusanOliveira <susan.kitsune147@gmail.com>
2021-04-10 14:43:42 -0300

    Projeto
    O restante do projeto

--- Corrida_Grilo/src/corrida_Grilo/ThreadProcessor.java
+++ Corrida_Grilo/src/corrida_Grilo/ThreadProcessor.java
@@ -1,10 +1,13 @@
 package corrida_Grilo;
 
+import java.util.Random;
+
 public class ThreadProcessor extends Thread {
 
 	Grilo [] grilo;
 	
 	int start, end, chegada;
+	Random r = new Random();
 	
 	public ThreadProcessor(Grilo [] grilo, int start, int end, int chegada) {
 		// TODO Auto-generated constructor stub
@@ -19,9 +22,19 @@ public class ThreadProcessor extends Thread {
 		// TODO Auto-generated method stub
 		
 		for(int c = start; c < end; c++) {
-			grilo[c].corrida(grilo[c], chegada, c);;
+			while(!grilo[c].getChegada()) {
+				grilo[c].setPecurso(r.nextInt(10-1) + 1);
+				System.out.println("O Grilo " + (c + 1) + " pulou " + grilo[c].getPulo() + "cm e já percorreu " + grilo[c].getPecorre() + "cm");
+				
+				if(grilo[c].getPecorre() >= chegada) {
+					System.out.println("O Grilo " + (c + 1) + " alcançou a linha de chegada com "+ grilo[c].getContP() +" pulos");
+					grilo[c].setChegada(true);					
+				}
+			}
 		}
+	}
+
 		
 		//super.run();
-	}
 }
+
