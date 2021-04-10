60e822599235a38831989e355a22cfc42aa8bf55
SusanOliveira <81330323+SusanOliveira@users.noreply.github.com>
2021-04-09 22:42:29 -0300

    Update Grilo.java
    Adição do método corrida.

--- Corrida_Grilo/src/corrida_Grilo/Grilo.java
+++ Corrida_Grilo/src/corrida_Grilo/Grilo.java
@@ -4,8 +4,8 @@ public class Grilo {
 private int  pulo, contP, pecorre;
 private Boolean chegada = false;
 
-Grilo() {
-
+public Grilo() {
+	
 }
 
 
@@ -34,4 +34,16 @@ public Boolean getChegada() {
 	return chegada;
 }
 
+public  void corrida(Grilo grilo, int chegada, int n) {
+	while(!grilo.getChegada()) {
+		if (grilo.getPecorre() <= chegada) {
+			grilo.setPecurso(r.nextInt(10-1) + 1);
+			System.out.println("O Grilo " + (n + 1) + " pulou " + grilo.pulo + "cm e já percorreu " + grilo.pecorre + "cm");
+		}else {
+			System.out.println("O Grilo " + (n + 1) + " alcançou a linha de chegada com "+ grilo.contP +" pulos");
+			grilo.setChegada(true);
+		}
+	}					
+}
+	
 }
