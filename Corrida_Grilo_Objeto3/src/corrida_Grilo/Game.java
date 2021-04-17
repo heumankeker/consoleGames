52876eb83cbb32e5aa7ba4f3ad5dbe90c6423873
SusanOliveira <susan.kitsune147@gmail.com>
2021-04-17 16:48:49 -0300

    Times
    Separação de times, soma do caminho percorrido e pulos, com time vencedor.

--- Corrida_Grilo_Objeto3/src/corrida_Grilo/Game.java
+++ Corrida_Grilo_Objeto3/src/corrida_Grilo/Game.java
@@ -3,7 +3,7 @@ package corrida_Grilo;
 
 public class Game {
 	
-	private int chegada, q;
+	private int chegada, q, timePercorrido, timePulo;
 	ThreadProcessor winner, vice;
 	
 	public void corrida(){
@@ -46,6 +46,27 @@ public class Game {
 	    	 		System.out.println( "O Grilo "+ c + " empatou com " + threads[c].contP + " pulos");
 			}
         }
+	 	
+	    int corrente = 0; 
+	    int timeVencedor = 0;
+	    
+	    for(int c = 0; corrente < threads.length; c++)
+	    {
+	    	timePercorrido = 0;
+	    	timePulo = 0;
+		    for(int l = 0; l < 3; l++) 
+		    {
+		    	timePercorrido += threads[corrente].percorre;
+		    	timePulo += threads[corrente].contP;
+		    	if(corrente == winner.n) 
+		    	{	  
+		    		timeVencedor = c;
+		    	}
+		    	corrente++;
+		    }  
+		    System.out.println("O Time " + (c + 1) +" percorreu "+ timePercorrido +"cm com o total de " + timePulo + " pulos");
+	    }
+	    System.out.println("O Time " + (timeVencedor + 1) + " foi o ganhador.");
 	}
 
 }
