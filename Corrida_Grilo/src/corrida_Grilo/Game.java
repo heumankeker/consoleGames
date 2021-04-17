a6389835b42455446b033c17e083d2d93089a2e4
SusanOliveira <susan.kitsune147@gmail.com>
2021-04-17 15:27:12 -0300

    Objeto 3
    Retirado a classe grilo e colocado na classe ThreadProcessor

--- Corrida_Grilo/src/corrida_Grilo/Game.java
+++ Corrida_Grilo/src/corrida_Grilo/Game.java
@@ -7,27 +7,25 @@ public class Game {
 	
 	public void corrida(){
 		chegada =  50;
-		q = 4;
-	
+		q = 12;
 		
 		int nThreads = q;
 		
 		ThreadProcessor [] threads = new ThreadProcessor[nThreads];
-	
 		for (int c = 0; c < q; c++) {
 			threads[c] = new ThreadProcessor(chegada, c);
 			threads[c].start();
 		}	
+
 		
-			for(int c = 0; c < nThreads; c++) {
-				try {
-					threads[c].join();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		for(int c = 0; c < nThreads; c++) {
+			try {
+				threads[c].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		
+		}
 	}
 
 }
--- Corrida_Grilo/src/corrida_Grilo/ThreadProcessor.java
+++ Corrida_Grilo/src/corrida_Grilo/ThreadProcessor.java
@@ -1,23 +1,41 @@
 package corrida_Grilo;
 
-public class ThreadProcessor extends Thread {
+import java.util.Random;
 
-	Grilo grilo;
+public class ThreadProcessor extends Thread {
 	
 	int chegada, n;
+	int  pulo, contP, percorre;
+	Random r = new Random();
+	private Boolean fim = false;
+	
 	
 	public ThreadProcessor(int chegada, int n) {
 		// TODO Auto-generated constructor stub
-		this.grilo = new Grilo();
 		this.chegada = chegada;
-		this.n = n;
+		this.n = n + 1;
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		grilo.corrida(grilo, chegada, n);
-		
+		while(fim == false) {
+			pulo = r.nextInt(10-1) + 1;	
+			if (percorre<= chegada) {
+				percorre += pulo;
+				contP++;
+				System.out.println("O Grilo " + n + " pulou " + pulo + "cm e já percorreu " + percorre + "cm");
+				/*try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}*/
+			}else {
+				fim = true;
+				System.out.println("O Grilo " + n + " alcançou a linha de chegada com "+ contP +" pulos");
+			}
+		}
 	}		
 		//super.run();
 }
